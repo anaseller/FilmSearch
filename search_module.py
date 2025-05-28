287183e26257888dde1d2b6fe5d09574ad593c1c
anaseller <seller.anst@gmail.com>
2025-05-28 09:39:52 +0200

    new commit

--- search_module.py
+++ search_module.py
@@ -15,7 +15,7 @@ class SearchModule:
 
     def combined_search(self, keyword=None, genre=None, year=None):
         query = '''
-            SELECT f.title, f.release_year, f.description, c.name AS category
+            SELECT DISTINCT f.title, f.release_year, f.description, c.name AS category
             FROM film f
             JOIN film_category fc ON f.film_id = fc.film_id
             JOIN category c ON fc.category_id = c.category_id
